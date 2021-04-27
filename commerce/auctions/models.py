e2d0307faa6d137dc1ab17261dc7592873500db4
HectorSchau <schauvinhold@gmail.com>
2021-04-27 08:41:39 -0300

    trying

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,13 +6,18 @@ class User(AbstractUser):
     pass
 
 class Auctions(models.Model):
-    auc_id=models.CharField(max_length=20)
+    auc_id=models.CharField(max_length=20, primary_key=True)
     auc_title=models.CharField(max_length=50)
-    auc_desc=models.TextField
-    auc_startbid=models.FloatField
-    auc_currentprice=models.FloatField
+    auc_desc=models.CharField(max_length=250, default="")
+    auc_startbid=models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    auc_currentprice=models.DecimalField(max_digits=10, decimal_places=2, default=0)
     auc_status=models.CharField(max_length=1)
+    auc_datestart=models.DateTimeField(default="")
     cat_id=models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.auc_id}: {self.auc_title}"
+
 
 
 
