c15c87316c54a0cb2d04e6439a90056b1e9db904
HectorSchau <schauvinhold@gmail.com>
2021-05-02 10:40:42 -0300

    fixing

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Auctions, User
 
 # Register your models here.
@@ -7,7 +6,3 @@ from .models import Auctions, User
 admin.site.register(Auctions)
 admin.site.register(User)
 
-=======
-
-# Register your models here.
->>>>>>> 18cb670ed0d224e9ed7aa583d8f5040a7ca94d9b
